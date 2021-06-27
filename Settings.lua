4ab70cc03d79f3c924a037f2215bb6d39412d474
Devibility <82962569+Devibility@users.noreply.github.com>
2021-06-27 22:46:42 +0530

    Add files via upload

---
+++ Settings.lua
@@ -0,0 +1,8 @@
+local Types = {}
+
+Types.Size = Vector3.new(4,4,4) -- You can change the values to whatever you like.
+Types.Duration = 5 -- You can change this value to whatever you like.
+Types.EasingStyle = Enum.EasingStyle.Linear -- You can change the Style to whatever you like.
+Types.EasingDirection = Enum.EasingDirection.In -- You can change the Direction to whatever you like.
+
+return Types
