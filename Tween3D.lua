84beeed99f53a41d554f2fa7c15f402dd3d3b638
Devibility <82962569+Devibility@users.noreply.github.com>
2021-06-27 22:44:58 +0530

    Add files via upload

---
+++ Tween3D.lua
@@ -0,0 +1,25 @@
+local Tween3D = {}
+local TweenService = game:GetService("TweenService")
+local Settings = require(script.Settings)
+local Maid = require(script.Maid)
+
+local Info = TweenInfo.new(Settings.Duration, Settings.EasingStyle, Settings.EasingDirection)
+local Props = {Size = Settings.Size}
+
+function Tween3D:PlayTween(obj)
+	coroutine.wrap(function()
+		xpcall(function()
+			local CreateTween = TweenService:Create(obj, Info, Props)
+			CreateTween:Play()
+			if Enum.TweenStatus.Completed then
+				local success = "%s was completed successfully!"
+				print(string.format(success, obj.Name .. " Tween"))
+			elseif Enum.TweenStatus.Canceled then
+				local e = "%s failed to complete!"
+				warn(string.format(e, obj.Name .. " Tween")) 
+			end
+		end, warn)
+	end)()
+end
+
+return Tween3D
